b37270b742dd726596543c0c9d93a0ae99a88175
Phillip viens <phillip.viens@gmail.com>
2017-04-04 14:47:31 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -1,1 +1,7 @@
-This is a comment
+Linux commands for
+
+pwd     # stands for 'Print working Directory' and tells me wehre I am (WHERE)
+whoami  # Tells me which user I'm logged in as (WHO)
+ls      # Tells me which files are in the directory (WHERE)
+cd      # Allows me to change in to other directories (NAVIGATION)
+date    # Tells me what time it is on the machine (WHEN)
